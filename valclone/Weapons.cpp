f1bdeb227f508e40e65ab429d2cdae181cb08b46
pratham264 <psangam2002@gmail.com>
2026-05-02 13:17:17 +0530

    Refactor Weapons class to use Weapon objects

--- valclone/Weapons.cpp
+++ valclone/Weapons.cpp
@@ -1,28 +1,26 @@
 #include "Weapons.h"
 
 Weapons::Weapons() {
-    string tempNames[8] = {
-        "Classic", "Ghost", "Sheriff",
-        "Judge", "Marshall",
-        "Vandal", "Phantom", "Operator"
-    };
-
-    int tempPrices[8] = {
-        0, 500, 800,
-        1850, 950,
-        2900, 2900, 4700
-    };
-
-    for (int i = 0; i < 8; i++) {
-        names[i] = tempNames[i];
-        prices[i] = tempPrices[i];
-    }
+    count = 8;
+    weapons[0] = Weapon("Classic",  0,    26);
+    weapons[1] = Weapon("Ghost",    500,  30);
+    weapons[2] = Weapon("Sheriff",  800,  55);
+    weapons[3] = Weapon("Judge",    1850, 60);
+    weapons[4] = Weapon("Marshall", 950,  45);
+    weapons[5] = Weapon("Vandal",   2900, 40);
+    weapons[6] = Weapon("Phantom",  2900, 39);
+    weapons[7] = Weapon("Operator", 4700, 150);
 }
 
-void Weapons::showWeapons() {
-    for (int i = 0; i < 8; i++) {
-        cout << i + 1 << ". " << names[i]
-             << " (" << prices[i] << ")\n";
+void Weapons::showWeapons() const {
+    for (int i = 0; i < count; i++) {
+        weapons[i].display(i);
     }
-    cout << "0. Move to next round\n"; 
+    cout << "0. Move to next round\n";
 }
+
+Weapon Weapons::getWeapon(int index) const {
+    return weapons[index];
+}
+
+int Weapons::getCount() const { return count; }
